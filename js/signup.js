2c9fde63443cca37e2e5d48b26aed906a4169359
lola2302 <lola2302@student.miun.se>
2025-03-16 16:04:02 +0100

    shared files

--- js/signup.js
+++ js/signup.js
@@ -11,13 +11,6 @@ async function handleSignup(username, password, email) {
         });
 
         const data = await response.json();
-        if (response.ok) {
-            // Call API to create the Google Drive folder for this user
-            await fetch('/api/create-folder', {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ username })
-            });
             updateMessage("Registrering lyckades. Du blir strax omdirigerad till inloggningssidan", true);
             setTimeout(() => {
                 window.location.href = "login.html";
