c82421c94aefccfe2d953037160bae11de5cf430
lola2302 <lola2302@student.miun.se>
2025-03-19 23:05:57 +0100

    chat update

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const pool = new Pool({
 
 const PgSession = pgSession(session);
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3006;
 
 const server = createServer(app); // Wrap Express with HTTP
 const io = new Server(server, {
